4d47dd0fb551d57efc350b3f4860e3ac884f4c64
unknown <szep.rydzewski@gmail.com>
2023-09-14 21:28:03 +0200

    done i think

--- server13.js
+++ server13.js
@@ -1,4 +1,3 @@
-'use strict';
 const express = require('express');
 const app = express();
 var cookieSession = require('cookie-session');
@@ -23,35 +22,42 @@ app.get('/', (req, res) => {
 		let old = Data.slice(20, 25);
 		let mins = old.slice(0, 2);
 		let sec = old.slice(3, 6);
-
 		req.session.date = new Date().toString();
 		let Data2 = JSON.stringify(req.session.date);
 		let old2 = Data2.slice(20, 25);
 		let mins2 = old2.slice(0, 2);
 		let sec2 = old2.slice(3, 6);
-
 		let lastmin = Number(mins2) - Number(mins);
 		let lastsec = Number(sec2) - Number(sec);
-
 		if (lastsec < 0) {
 			lastsec = Number(sec) - Number(sec2);
 		}
-
-		console.log(old3);
-		console.log(mins3);
-		console.log(sec3);
-		let firstmin = Number(mins) - Number(mins3);
-		let firstsec = Number(sec) - Number(sec3);
-
+		// console.log(old3);
+		// console.log(mins3);
+		// console.log(sec3);
+		let firstmin = Number(mins + sec) - Number(mins3 + sec3);
+		let firstsec;
+		if (firstmin.length === 3) {
+			firstsec = firstmin.slice(-2);
+			firstmin = firstmin.slice(0, 1);
+			console.log(firstmin);
+		} else if (firstmin.length === 4) {
+			firstsec = firstmin.slice(-2);
+			firstmin = firstmin.slice(0, 2);
+		} else {
+			firstsec = firstmin;
+			firstmin = 0;
+		}
 		res.send(
 			`you first visited us ${firstmin} minutes and ${firstsec} seconds ago
-            <br> you last visited us ${lastmin} minutes and ${lastsec} seconds ago`
+	        <br> you last visited us ${lastmin} minutes and ${lastsec} seconds ago`
 		);
+		return;
 	} else {
 		req.session.date = new Date().toString();
+		return;
 	}
-
-	//res.send('site');
+	res.send('site');
 });
 
 app.listen(3000, () => console.log('running'));
