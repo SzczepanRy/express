1de4fd4c37c7cea9f99a08fa66c519a02de7a13e
unknown <szep.rydzewski@gmail.com>
2023-10-03 21:02:20 +0200

    a

---
+++ static/l3/server8.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+app.post('/call', (req, res) => {
+	const { x, y } = req.body;
+	res.json({ x: x, y: y });
+});
+
+app.listen(3000, () => {
+	console.log('runnin');
+});
